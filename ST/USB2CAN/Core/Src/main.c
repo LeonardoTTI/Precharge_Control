df2b2575986e0cca79f23815cfd9895ef069190b
Leonardo Mugnetti <leonardo.mugnetti.00@gmail.com>
2024-11-30 12:24:29 +0100

    USB2CANFD VCOM feature: implemented the RX in main
    TODO: CANFD send

--- ST/USB2CAN/Core/Src/main.c
+++ ST/USB2CAN/Core/Src/main.c
@@ -34,7 +34,8 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+uint32_t rx_len;
+uint8_t USBRX_buffer[69];
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -91,7 +92,7 @@ int main(void)
   MX_FDCAN1_Init();
   MX_USB_Device_Init();
   /* USER CODE BEGIN 2 */
-
+  CDC_Init_FS();
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -99,7 +100,10 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+	  rx_len = CDC_Receive_FS(USBRX_buffer, 69);
+	  if(rx_len>0){
+		  //todo implementare invio in CAN bus
+	  }
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
